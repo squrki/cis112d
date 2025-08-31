39c6a3dc85ac5c2524d5cad0f0abeb57451ca510
Kirn Kim <squrkidev@gmail.com>
2025-08-30 17:10:35 -0700

    initial commit

---
+++ template.java
@@ -0,0 +1,14 @@
+/**
+ * PURPOSE:
+ * PARAMETERS:
+ * RETURN VALUES:
+ */
+
+/**
+ * PURPOSE: displays when the cursor lingers over the component. If the text is null, the tool tip is turned off for this component.
+ * PARAMETERS:
+ *     str1, the string to display.
+ *     str2, the string to carry over to next iterative function, nextFunc()
+ * RETURN VALUES:
+ *     The combined string after cursor is done moving
+*/
