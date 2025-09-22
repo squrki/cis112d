f73e3235a2fea349f2844bb349fe278172af29d9
Kirn Kim <squrkidev@gmail.com>
2025-09-22 00:00:30 -0700

    streak Mon Sep 22 00:00:30 PDT 2025

--- Lab14.java
+++ Lab14.java
@@ -5,7 +5,7 @@ public class Lab14 {
         Scanner scnr = new Scanner(System.in);
         int num = scnr.nextInt();
         String roman = new String();
-        int place = 1;
+        // int place = 1;
         while (num > 0) {
             System.out.printf("num: %d, place: %d\n", num, place);
             // if place goes into num add to string
@@ -15,41 +15,51 @@ public class Lab14 {
             // continue;
             // }
 
-            switch (place) {
-                case 1:
-                    if (num % 2 == 1) {
-                        roman += "1";
-                        num -= 1;
-                    } else {
-                        roman += "0";
-                    }
-                    place *= 2;
-                    break;
-                case 2:
-                    if (num / 2 == 1) {
-                        roman += "1";
-                        num -= 2;
-                    } else {
-                        roman += "0";
-                    }
-                    place *= 2;
-                    break;
-                default:
-                    if (num / place == 1) {
-                        roman += "1";
-                        num -= place;
-                    } else {
-                        roman += "0";
-                    }
-                    place *= 2;
-                    break;
+            // switch (place) {
+            // case 1:
+            if (num % 2 == 1) {
+                roman += "1";
+                num -= 1;
+            } else {
+                roman += "0";
             }
+            if (num / place == 1) {
+                roman += "1";
+                num -= place;
+            } else {
+                roman += "0";
+            }
+            place *= 2;
+            break;
+
+            // place *= 2;
+            // break;
+            // case 2:
+            // if (num / 2 == 1) {
+            // roman += "1";
+            // num -= 2;
+            // } else {
+            // roman += "0";
+            // }
+            // place *= 2;
+            // break;
+            // default:
+            // if (num / place == 1) {
+            // roman += "1";
+            // num -= place;
+            // } else {
+            // roman += "0";
+            // }
+            // place *= 2;
+            // break;
+            // }
             // num -= place;
             // if (place > 2) {
             // place *= 2;
             // }
             System.out.printf("roman: %s\n", roman);
         }
+        scnr.close();
         System.out.println(roman);
     }
 }
