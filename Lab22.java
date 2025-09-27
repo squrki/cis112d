55ea548d461fe4faf1508e394e568da094be6a36
Kirn Kim <squrkidev@gmail.com>
2025-09-26 22:15:06 -0700

    lab 22

---
+++ Lab22.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Lab22 {
+   public static void main(String[] args) {
+      Scanner scnr = new Scanner(System.in);
+      String[] words = new String[20];
+
+      int numElements = scnr.nextInt();
+
+      for (int i = 0; i < numElements; i++) {
+         words[i] = scnr.next();
+      }
+
+      String ch = scnr.next();
+
+      for (int i = 0; i < numElements; i++) {
+         if (words[i].contains(ch)) {
+            System.out.printf("%s,", words[i]);
+         }
+      }
+      System.out.println();
+
+      scnr.close();
+
+   }
+}
