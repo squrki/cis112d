443d6c47b94ee9572c50405673c25ad9d762a864
Kirn Kim <squrkidev@gmail.com>
2025-09-28 11:24:27 -0700

    hw 3 Sun Sep 28 11:24:27 PDT 2025

--- HW3.java
+++ HW3.java
@@ -1,18 +1,3 @@
-/**
- * PURPOSE:
- * PARAMETERS:
- * RETURN VALUES:
- */
-
-/**
- * PURPOSE: displays when the cursor lingers over the component. If the text is null, the tool tip is turned off for this component.
- * PARAMETERS:
- *     str1, the string to display.
- *     str2, the string to carry over to next iterative function, nextFunc()
- * RETURN VALUES:
- *     The combined string after cursor is done moving
-*/
-
 public class HW3 {
     public static void main(String[] args) {
         // your solution method may be tested as such, with a random linked list input
@@ -22,10 +7,11 @@ public class HW3 {
         node.next.next.next = new Node(4);
         node.next.next.next.next = null;
         Solution sol = new Solution();
-        sol.mixList(node); // list becomes homogeneous after this call. See below hint. 
+        sol.mixList(node); // list becomes homogeneous after this call. See below hint.
         // you may use printList(node) to print out the list content after mixing.
         printList(node);
     }
+
     public static void printList(Node head) {
         Node node = head;
         while (node.next != null) {
@@ -35,21 +21,54 @@ public class HW3 {
         System.out.println(node.val + "->null");
     }
 }
+
 class Node {
     int val;
     Node next;
+
     Node(int x) {
         val = x;
     }
 }
 
 /**
- * PURPOSE: 
- * PARAMETERS: 
- * RETURN VALUES:
+ * PURPOSE: Converts non-homogeneous linked list to a homogeneous linked list.
+ * PARAMETERS: head, the head of the linked list to convert
+ * RETURN VALUES: None.
  */
+
 class Solution {
     public void mixList(Node head) {
-        // YOUR CODE HERE
+        // if list is single node, return
+        if (head.next == null) {
+            return;
+        }
+        // mark current Node
+        Node currentNode = head;
+
+        // find last node and 2nd to last node
+
+        while (currentNode.next != null) {
+            Node secondLastNode = currentNode;
+            Node lastNode = currentNode.next;
+            while (lastNode.next != null) {
+                secondLastNode = lastNode;
+                lastNode = lastNode.next;
+            }
+            // assign node.next() to last node.next()
+            lastNode.next = currentNode.next;
+
+            // assign last node to node.next()
+            currentNode.next = lastNode;
+
+            // assign original node.next() to currentNode
+            currentNode = lastNode.next;
+
+            // make 2nd last node to last node and assign null to next
+            // lastNode = secondLastNode;
+            // lastNode.next = null;
+            secondLastNode.next = null;
+        }
+
     }
 }
