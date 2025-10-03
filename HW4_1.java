9cc64ecea500c017d007de6d995c7e50d40031ad
Kirn Kim <squrkidev@gmail.com>
2025-10-02 19:28:01 -0700

    start hw4=1

---
+++ HW4_1.java
@@ -0,0 +1,20 @@
+public class HW4_1 {
+    public static void main(String[] args) {
+        // your solution will be tested as such, sequentially with random input
+        Solution sol = new Solution();
+        sol.canDistribute(new int[] { 1 }, 2);
+        sol.canDistribute(new int[] { 1, 2, 2, 3, 3, 5, 5 }, 12);
+        // etc.
+    }
+}
+
+/**
+ * PURPOSE:
+ * PARAMETERS:
+ * RETURN VALUES:
+ */
+class Solution {
+    public boolean canDistribute(int[] resources, int groups) {
+        // YOUR CODE HERE
+    }
+}
