96f0c74f6b6c1c40090ac4d33026214758c1c670
Kirn Kim <squrkidev@gmail.com>
2025-10-04 15:32:29 -0700

    hw4-1 done, with comment old solutions

--- HW4_1.java
+++ HW4_1.java
@@ -4,12 +4,14 @@ public class HW4_1 {
     public static void main(String[] args) {
         // your solution will be tested as such, sequentially with random input
         Solution sol = new Solution();
-        sol.canDistribute(new int[] { 1 }, 2);
-        sol.canDistribute(new int[] { 1, 2, 2, 3, 3, 5, 5 }, 12);
-        sol.canDistribute(new int[] { 3, 4, 5, 6 }, 2); // true
-        sol.canDistribute(new int[] { 1 }, 1); // true
-        sol.canDistribute(new int[] { 1, 3, 2, 3, 4, 1, 3, 5, 2, 1 }, 5); // true
-        sol.canDistribute(new int[] { 1 }, 4); // false
+        System.out.println(sol.canDistribute(new int[] { 3, 4, 5, 6 }, 2)); // true
+        System.out.println(sol.canDistribute(new int[] { 1 }, 1)); // true
+        System.out.println(sol.canDistribute(new int[] { 1, 3, 2, 3, 4, 1, 3, 5, 2, 1 }, 5)); // true
+        System.out.println(sol.canDistribute(new int[] { 5, 4, 3, 2, 2 }, 2)); // true
+        System.out.println(sol.canDistribute(new int[] { 4, 5, 6, 5 }, 2)); // true
+        System.out.println(sol.canDistribute(new int[] { 1 }, 2)); // false
+        System.out.println(sol.canDistribute(new int[] { 1, 2, 2, 3, 3, 5, 5 }, 12)); // false
+        System.out.println(sol.canDistribute(new int[] { 4, 3, 2, 3, 5, 2, 1 }, 3)); // false
         // etc.
     }
 }
@@ -19,7 +21,7 @@ public class HW4_1 {
  * facilities
  * PARAMETERS: resources - positive integer array, groups - 'k' number of
  * facilities
- * RETURN VALUES:
+ * RETURN VALUES: boolean on if the distribution is possible.
  */
 class Solution {
     public boolean canDistribute(int[] resources, int groups) {
@@ -37,11 +39,6 @@ class Solution {
         // SUM(resources)/k.
         int allocation = Arrays.stream(resources).sum();
 
-        // int allocation = 0;
-        // for (int r : resources) {
-        // allocation += r;
-        // }
-
         // check if resources can be evenly divided
         if (allocation % groups != 0) {
             return false;
@@ -58,64 +55,117 @@ class Solution {
             resources[resources.length - 1 - i] = temp;
         }
 
-        // cannot evenly distribute resource larger than average
+        // Check if the selected resource is <= allocation:
         if (resources[0] > allocation)
             return false;
 
         // Create another solution method of your choice to enable recursion.
-        int[] divisions = new int[groups];
+        // Create an empty array of integers with k elements.
+        int[] buffer = new int[groups];
+
+        // Pick the highest resource value in your resources.
         int resIndex = 0;
-        return canPartition(resources, divisions, resIndex, allocation);
+
+        return checkDivs(resources, buffer, resIndex, allocation);
+
+        // buffer = checkDivs(resources, buffer, resIndex, bufIndex, allocation);
+
+        // // 5. Check if every element in your memory buffer has the same value. If so
+        // you
+        // // have an evenly allocated distribution - return true. False otherwise.
+        // for (int b : buffer) {
+        // if (b != allocation)
+        // return false;
+        // }
+
+        // return true;
     }
 
-    private boolean canPartition(int[] resources, int[] partitions, int resourceIndex, int k) {
-        if (resourceIndex >= resources.length)
+    /**
+     * PURPOSE: Recursing helper function for canDistribute()
+     * PARAMETERS:
+     * int[] res - resources
+     * int[] buf - memory buffer of allocations
+     * int resInd - current index of res
+     * int alloc - allocation to each buf element
+     * RETURN VALUES: boolean - is possible to allocate
+     */
+    boolean checkDivs(int[] res, int[] buf, int resInd, int alloc) {
+        // base case
+        if (resInd >= res.length)
             return true;
 
-        int currentResource = resources[resourceIndex];
+        if (res[resInd] <= alloc) {
+            for (int i = 0; i < buf.length; i++) {
+                // Check if the selected resource + current buffer element <= allocation:
+                if (buf[i] + res[resInd] <= alloc) {
+                    // add next highest value to the first available element in your memory buffer
+                    // of k elements. Go to #4.
+                    buf[i] += res[resInd];
+                    // move to next resource
+                    resInd++;
+                    break;
+                }
+            }
 
-        for (int i = 0; i < k - 1; i++) {
-
+        } else {
+            // If not, move on (return false) to allow recursion to go back, servicing the
+            // recursive calls already in your memory buffer.
+            // Note: if there's no available space to accommodate the selected resource, it
+            // is impossible for resources to be allocated equally.
+            return false;
         }
-        return true;
+        return checkDivs(res, buf, resInd, alloc);
 
-        // Remaining Pseudo Code:
-
-        // Create an empty array of integers with k elements. This is your "memory
-        // buffer". At the end of your recursion, you want this memory buffer be filled
-        // with equally allocated values, allocation.
-        // int[] buf = new int[grps];
-        // int bufPlace = 0;
-        // int resStart = 0;
-        // int resEnd = subRes.length - 1;
-
-        // Pick the highest resource (one with the highest integral value) in your
-        // resources.
-        // 3. Check if the selected resource is <= allocation:
-        // if (subRes[resEnd] == allocation) {
-        // If yes, add that value to the first available element in your memory buffer
-        // of k elements. Go to #4.
-        // buf[0] = subRes[resEnd];
-        // bufPlace++;
-        // resEnd--;
-        // } else if (subRes[resEnd] < allocation) {
-        // if (subRes[0] + subRes[resEnd] == allocation) {
-        // buf[0] = subRes[0] + subRes[resEnd];
-        // }
-        // } else {
-        // If not, move on (return false) to allow recursion to go back, servicing the
-        // recursive calls already in your memory buffer.
-        // return buf;
-        // Note: if there's no available space to accommodate the selected resource, it
-        // is impossible for resources to be allocated equally.
     }
-    // 4. Advance to the next highest resource. Repeat Step #3. If all resources
-    // have
-    // been considered and there is no more next highest, go to Step #5.
-    // System.arraycopy(resCheck(Arrays.copyOfRange(subRes, 1, resEnd), grps,
-    // allocation), 0, buf, 1, resEnd);
-
-    // 5. Check if every element in your memory buffer has the same value. If so you
-    // have an evenly allocated distribution - return true. False otherwise.
-
 }
+// int[] checkDivs(int[] res, int[] buf, int resInd, int bufInd, int alloc) {
+// // base case
+// if (resInd >= res.length)
+// return buf;
+
+// // Check if the selected resource + resource in buffer <= allocation:
+// if (buf[bufInd] + res[resInd] <= alloc) {
+// // If yes, add that value to memory buffer
+// buf[bufInd] += res[resInd];
+// // move to next resource
+// resInd++;
+// bufInd = 0;
+// } else {
+// // if > allocation, move to next buffer element or first buffer element
+// if (bufInd != buf.length - 1)
+// // bufInd = 0;
+// // else
+// bufInd++;
+// }
+
+// buf = checkDivs(res, buf, resInd, bufInd, alloc);
+
+// return buf;
+
+// }
+// }
+// boolean checkDivs(int[] res, int[] bufs, int resInd, int alloc) {
+
+// if (resInd >= res.length)
+// return true;
+
+// int k = buf.length;
+// int currentResource = res[resInd];
+
+// for (int i = 0; i < k; i++) {
+// if (currentResource + buf[i] <= alloc) {
+// buf[i] += currentResource;
+// if (checkDivs(res, buf, resInd + 1, alloc)) {
+// return true;
+// } else {
+// buf[i] -= currentResource;
+// }
+// }
+// if (buf[i] == 0) {
+// break;
+// }
+// }
+
+// return false;
+// }
