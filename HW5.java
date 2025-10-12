238f3a5314bf1beed7068999b4a21e57f5f4006b
Kirn Kim <squrkidev@gmail.com>
2025-10-12 12:18:10 -0700

    HW 5 in progress

--- HW5.java
+++ HW5.java
@@ -5,16 +5,17 @@ public class HW5 {
       // your solution will be tested w/ random add()'s and remove()'s.
       // Observers like peek(), isEmpty() will be used along the way to validate.
       Solution sol = new Solution();
-      sol.reverse()  // no effect since the queue is initially empty
-      sol.add(8); 
-      sol.add(1); 
-      sol.reverse()  // this reverses the order of elements 8->1 to 1->8 in queue
-      sol.peek();    // 1, if you use System.out.print()
-      sol.remove();  // 1
+      sol.reverse(); // no effect since the queue is initially empty
+      sol.add(8);
+      sol.add(1);
+      sol.peek(); // 8, if you use System.out.print() ** REMOVE
+      sol.reverse(); // this reverses the order of elements 8->1 to 1->8 in queue
+      sol.peek(); // 1, if you use System.out.print()
+      sol.remove(); // 1
       sol.isEmpty(); // false
-      sol.add(2); 
-      sol.add(3); 
-      sol.peek();    // 8
+      sol.add(2);
+      sol.add(3);
+      sol.peek(); // 8
       // etc
    }
 }
@@ -23,6 +24,8 @@ class Solution {
    // PLEASE USE THESE GLOBAL STACKS FOR THIS PROBLEM
    private Stack<Integer> pushStack = new Stack<Integer>();
    private Stack<Integer> popStack = new Stack<Integer>();
+   // Stack<Integer> pushStack = new Stack<Integer>();
+   // Stack<Integer> popStack = new Stack<Integer>();
    /*
     * ====================================
     * !!! DO NOT MODIFY ABOVE THIS LINE !!!
@@ -35,7 +38,15 @@ class Solution {
     * RETURN VALUES: None
     */
    public void add(int x) {
-      // YOUR CODE HERE
+      // while (!this.pushStack.empty()) {
+      // this.popStack.push(this.pushStack.pop());
+      // }
+      // this.popStack.push(x);
+      // while (!this.popStack.empty()) {
+      // this.pushStack.push(this.popStack.pop());
+      // }
+      this.pushStack.push(x);
+      System.out.println(this.pushStack);
       return;
    }
 
@@ -45,7 +56,10 @@ class Solution {
     * RETURN VALUES: integer
     */
    public int remove() {
-      return 1;
+      // this.popStack.push(this.pushStack.pop());
+      // return this.popStack.peek();
+      System.out.println("removing: " + this.pushStack.peek());
+      return this.pushStack.pop();
    }
 
    /**
@@ -54,8 +68,8 @@ class Solution {
     * RETURN VALUES: integer
     */
    public int peek() {
-      // YOUR CODE HERE
-      return 1;
+      System.out.println("peeking: " + this.pushStack.peek());
+      return this.pushStack.peek();
    }
 
    /**
@@ -64,7 +78,8 @@ class Solution {
     * RETURN VALUES:
     */
    public boolean isEmpty() {
-      // YOUR CODE HERE
+      System.out.println(this.pushStack.empty());
+      return this.pushStack.empty();
    }
 
    /**
@@ -73,7 +88,20 @@ class Solution {
     * RETURN VALUES:
     */
    public void reverse() {
-      // YOUR CODE HERE
+      System.out.println("pushStack: " + this.pushStack);
+      while (!this.pushStack.empty()) {
+         this.popStack.push(this.pushStack.pop());
+      }
+      System.out.println("pushStack: " + this.pushStack + "  |  popStack: " + this.popStack);
+      while (!this.popStack.empty()) {
+         // this.pushStack.push(this.popStack.pop());
+         System.out.println(this.popStack.lastElement());
+         this.pushStack.push(this.popStack.lastElement());
+         this.popStack.removeElementAt(this.popStack.size() - 1);
+         System.out.println(this.popStack);
+      }
+      System.out.println("pushStack: " + this.pushStack + "  |  popStack: " + this.popStack);
+
    }
 
 }
