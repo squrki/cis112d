398db62c74adccd4d5245c532a93838bcfd41661
Kirn Kim <squrkidev@gmail.com>
2025-10-15 15:20:33 -0700

    add HW6 starter code

--- HW6.java
+++ HW6.java
@@ -1,103 +1,101 @@
-import java.util.Stack;
-
 public class HW6 {
    public static void main(String[] args) {
-      // your solution will be tested w/ random add()'s and remove()'s.
-      // Observers like peek(), isEmpty() will be used along the way to validate.
-      Solution sol = new Solution();
-      sol.reverse(); // no effect since the queue is initially empty
+      // your solution method will be tested as such, with random sequential input
+      // TEST CASE #1: instantiate a queue of capacity = 1
+      Solution sol = new Solution(1);
+      sol.getFront(); // -1
+      sol.getRear(); // -1
       sol.add(8);
-      sol.add(1);
-      sol.reverse(); // this reverses the order of elements 8->1 to 1->8 in queue
-      sol.peek(); // 1, if you use System.out.print()
-      sol.remove(); // 1
-      sol.isEmpty(); // false
-      sol.add(2);
-      sol.add(3);
+      sol.getFront(); // 0
+      sol.getRear(); // 0
       sol.peek(); // 8
+      sol.remove(); // 8
+      sol.isEmpty(); // true
+      sol.getFront(); // -1, when queue is empty
+      sol.getRear(); // -1, when queue is empty
+
+      // TEST CASE #2: instantiate a queue of capacity = 3
+      Solution sol2 = new Solution(3);
+      sol2.add(1);
+      sol2.add(2);
+      sol2.add(3);
+      sol2.getFront(); // 0
+      sol2.getRear(); // 2
+      sol2.remove(); // 1
+      sol2.getFront(); // 1
+      sol2.getRear(); // 2
       // etc
    }
 }
 
 class Solution {
-   // PLEASE USE THESE GLOBAL STACKS FOR THIS PROBLEM
-   private Stack<Integer> pushStack = new Stack<Integer>();
-   private Stack<Integer> popStack = new Stack<Integer>();
-   /*
-    * ====================================
-    * !!! DO NOT MODIFY ABOVE THIS LINE !!!
-    * ====================================
-    */
+   // Dynamic array size
+   private int capacity;
+   // Queue
+   private int[] elements;
+   // Dynamic queue size
+   private int numElements = 0;
+   // Dynamic index for the front of queue, defaulting to -1 when the queue is
+   // empty
+   private int front = -1;
+   // Dynamic index for the rear of queue, defaulting to -1 when the queue is empty
+   private int rear = -1;
 
-   /**
-    * PURPOSE: add item to queue
-    * PARAMETERS: x: integer
-    * RETURN VALUES: None
+   // Constructor
+   public Solution(int capacity) {
+      this.capacity = capacity;
+      this.elements = new int[this.capacity];
+   }
+
+   // Get the front index
+   public int getFront() {
+      return this.front;
+   }
+
+   // Get the rear index
+   public int getRear() {
+      return this.rear;
+   }
+
+   /*
+    * =====================================
+    * /* !!! DO NOT MODIFY ABOVE THIS LINE !!!
+    * /* =====================================
+    * 
+    * 
+    * /**
+    * PURPOSE:
+    * PARAMETERS:
+    * RETURN VALUES:
     */
    public void add(int x) {
-
-      // move items to popStack
-      while (!this.pushStack.empty()) {
-         this.popStack.push(this.pushStack.pop());
-      }
-
-      // add item to top of popStack
-      this.popStack.push(x);
-
-      // move items to pushStack to maintain queue order
-      while (!this.popStack.empty()) {
-         this.pushStack.push(this.popStack.pop());
-      }
-      return;
+      // YOUR CODE HERE
    }
 
    /**
-    * PURPOSE: remove item from queue
-    * PARAMETERS: none
-    * RETURN VALUES: integer value of removed item in queue
+    * PURPOSE:
+    * PARAMETERS:
+    * RETURN VALUES:
     */
    public int remove() {
-      // removes first item of stack, which on pushStack is first item in queue
-      return this.pushStack.pop();
+      // YOUR CODE HERE
    }
 
    /**
-    * PURPOSE: see value of first item in queue
-    * PARAMETERS: none
-    * RETURN VALUES: integer value of first item in queue
+    * PURPOSE:
+    * PARAMETERS:
+    * RETURN VALUES:
     */
    public int peek() {
-      // returns first item of stack, which on pushStack is first item in queue
-      return this.pushStack.peek();
+      // YOUR CODE HERE
    }
 
    /**
-    * PURPOSE: check is queue is empty
-    * PARAMETERS: none
-    * RETURN VALUES: boolean of whether queue is empty
+    * PURPOSE:
+    * PARAMETERS:
+    * RETURN VALUES:
     */
    public boolean isEmpty() {
-      // checks whether queue (pushStack) is empty
-      return this.pushStack.empty();
+      // YOUR CODE HERE
    }
-
-   /**
-    * PURPOSE: reverse the order of the queue
-    * PARAMETERS: none
-    * RETURN VALUES: none
-    */
-   public void reverse() {
-      // move items from queue to popStack in reverse order
-      while (!this.pushStack.empty()) {
-         this.popStack.push(this.pushStack.elementAt(0));
-         this.pushStack.removeElementAt(0);
-      }
-
-      // move items from popStack back to queue
-      while (!this.popStack.empty()) {
-         this.pushStack.push(this.popStack.pop());
-      }
-
-   }
-
 }
