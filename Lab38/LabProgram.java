178258de97d0adf929807493120920db947f2fb6
Kirn Kim <squrkidev@gmail.com>
2025-11-06 12:39:37 -0800

    Lab38 Complete

--- Lab38/LabProgram.java
+++ Lab38/LabProgram.java
@@ -5,7 +5,16 @@ import java.io.IOException;
 public class LabProgram {
    public static void main(String[] args) throws IOException {
       Scanner scnr = new Scanner(System.in);
-
-      /* Type your code here. */
+      String filename = scnr.next();
+      FileInputStream file = new FileInputStream(filename);
+      Scanner inFS = new Scanner(file);
+      while (inFS.hasNext()) {
+         String data = inFS.nextLine();
+         String d = data.replace("photo.jpg", "info.txt");
+         System.out.println(d);
+      }
+      scnr.close();
+      inFS.close();
+      file.close();
    }
 }
