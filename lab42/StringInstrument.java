703334955083295cfee6480ed6e3804acff6eaf8
Kirn Kim <squrkidev@gmail.com>
2025-11-11 17:44:26 -0800

    lab43 complete

--- lab42/StringInstrument.java
+++ lab42/StringInstrument.java
@@ -1,13 +1,34 @@
 // TODO: Define a class: StringInstrument that is derived from the Instrument class
 public class StringInstrument extends Instrument {
    // TODO: Declare private fields
+   private int numStrings;
+   private int numFrets;
+   private boolean isBowed;
 
-   // TODO: Define mutator methods - 
-   //       setNumOfStrings(), setNumOfFrets(), setIsBowed()
+   // TODO: Define mutator methods -
+   public void setNumOfStrings(int n) {
+      numStrings = n;
+   }
 
+   public void setNumOfFrets(int n) {
+      numFrets = n;
+   }
 
-   // TODO: Define accessor methods - 
-   //       getNumOfStrings(), getNumOfFrets(), getIsBowed()
+   public void setIsBowed(boolean b) {
+      isBowed = b;
+   }
 
+   // TODO: Define accessor methods -
+   // getNumOfStrings(), getNumOfFrets(), getIsBowed()
+   public int getNumOfStrings() {
+      return numStrings;
+   }
+
+   public int getNumOfFrets() {
+      return numFrets;
+   }
+
+   public boolean getIsBowed() {
+      return isBowed;
+   }
 }
-
