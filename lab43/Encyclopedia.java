33a54c6c435a5bae2741591e17dde240b0cd7c75
Kirn Kim <squrkidev@gmail.com>
2025-11-11 17:51:07 -0800

    lab43 REALLY complete this time

--- lab43/Encyclopedia.java
+++ lab43/Encyclopedia.java
@@ -1,16 +1,38 @@
 public class Encyclopedia extends Book {
    // TODO: Declare private fields
-  
-   
-   // TODO: Define mutator methods - 
-   //       setEdition(), setNumPages()
-   
-  
+   private String edition;
+   private int numPages;
+
+   // TODO: Define mutator methods -
+   // setEdition(), setNumPages()
+   public void setEdition(String s) {
+      edition = s;
+   }
+
+   public void setNumPages(int n) {
+      numPages = n;
+   }
+
    // TODO: Define accessor methods -
-   //       getEdition(), getNumPages()
-   
-   
-   // TODO: Define a printInfo() method that overrides 
-   //       the printInfo in Book class 
-   
+   // getEdition(), getNumPages()
+   public String getEdition() {
+      return edition;
+   }
+
+   public int getNumPages() {
+      return numPages;
+   }
+
+   // TODO: Define a printInfo() method that overrides
+   // the printInfo in Book class
+   @Override
+   public void printInfo() {
+      System.out.println("Book Information: ");
+      System.out.println("   Book Title: " + title);
+      System.out.println("   Author: " + author);
+      System.out.println("   Publisher: " + publisher);
+      System.out.println("   Publication Date: " + publicationDate);
+      System.out.println("   Edition: " + edition);
+      System.out.println("   Number of Pages: " + numPages);
+   }
 }
