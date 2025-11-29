f5a401ed262567488acca6dac20bc6133d1b5389
Kirn Kim <squrkidev@gmail.com>
2025-11-28 20:27:45 -0800

    streak Fri Nov 28 20:27:45 PST 2025

--- lab47/NumberPattern.java
+++ lab47/NumberPattern.java
@@ -2,14 +2,17 @@ import java.util.Scanner;
 
 public class NumberPattern {
    // TODO: Write recursive printNumPattern() method
-  
-   public static void main(String[] args) { 
+   public static void printNumPattern(int num1, int num2) {
+
+   }
+
+   public static void main(String[] args) {
       Scanner scnr = new Scanner(System.in);
       int num1;
       int num2;
-      
+
       num1 = scnr.nextInt();
       num2 = scnr.nextInt();
-      printNumPattern(num1, num2); 
-   } 
-} 
+      printNumPattern(num1, num2);
+   }
+}
