83cc9c976043468f55240bc1a00243b614cf7ef6
Kirn Kim <squrkidev@gmail.com>
2025-12-01 19:48:25 -0800

    lab46 complete

--- lab46/PhotoLineups.java
+++ lab46/PhotoLineups.java
@@ -10,7 +10,6 @@ import java.util.ArrayList;
 
 // then the output is (must match the below ordering):
 
-
 // Julia, Lucas, Mia 
 // Julia, Mia, Lucas
 // Lucas, Julia, Mia
@@ -22,33 +21,78 @@ public class PhotoLineups {
 
    // TODO: Write method to create and output all permutations of the list of
    // names.
-   public static void printAllPermutations(ArrayList<String> permList, ArrayList<String> nameList) {
-      String name;
-      String line;
-      int i;
+   // public static void printAllPermutations(ArrayList<String> permList,
+   // ArrayList<String> nameList) {
 
-      if (nameList.size() == 0) {
-         System.out.println(permList);
+   public static void printAllPermutations(ArrayList<String> currentList, ArrayList<String> remainingList) {
+      // String name;
+      // String line = "";
+      // int i;
 
-      } else {
-         for (i = 0; i < nameList.size(); i++) {
-            name = nameList.get(i);
-            nameList.remove(i);
-            permList.add(name);
+      // if (nameList.size() == 0 || nameList.size() == 1) {
+      // System.out.println(nameList.toString());
+      // } else {
+      // for (i = 0; i < nameList.size(); i++) {
+      // for (int j = 0; j < nameList.size(); j++) {
+      // line.concat(nameList.get(i));
+      // if (j < nameList.size() -1) {
+      // line.concat(", ");
+      // }
+      // }
 
-            printAllPermutations(permList, nameList);
+      // name = nameList.get(i);
+      // tempList.addAll(nameList);
+      // tempList.remove(i);
 
-            nameList.add(i, name);
+      // printAllPermutations(permList, nameList);
 
-         }
+      // nameList.add(i, name);
+
+      // }
+      // }
+      // // for (String n : nameList)
+      // // for (int i = 0; i < nameList.size() - 1; i++)
+      // // permList.add(n);
+      // // }else
+
+      // // {
+      // for (String perm : permList) {
+      // System.out.println(perm);
+      // }
+      // Base Case: If there are no names left to choose from,
+      // the current list is a complete permutation. Print it.
+      if (remainingList.isEmpty()) {
+         // Output the permutation separated by a comma and space.
+         System.out.println(String.join(", ", currentList));
+         return;
       }
-      // for (String n : nameList)
-      // for (int i = 0; i < nameList.size() - 1; i++)
-      // permList.add(n);
-      // }else
 
-      // {
+      // Recursive Step: Iterate through all available names in the remainingList.
+      for (int i = 0; i < remainingList.size(); i++) {
 
+         // 1. CHOOSE: Select a name from the remaining list.
+         String chosenName = remainingList.get(i);
+
+         // 2. EXPLORE: Update the lists for the recursive call.
+
+         // Add the chosen name to the current line-up being built.
+         currentList.add(chosenName);
+
+         // Remove the name from the remaining pool using its index.
+         remainingList.remove(i);
+
+         // Recurse to fill the next spot in the line-up.
+         printAllPermutations(currentList, remainingList);
+
+         // 3. BACKTRACK: Undo the changes to the lists to try the next choice.
+
+         // Put the name back into the remaining list at the same index it was removed
+         // from.
+         remainingList.add(i, chosenName);
+
+         // Remove the name from the current line-up (it's always the last element).
+         currentList.remove(currentList.size() - 1);
+      }
    }
 
    public static void main(String[] args) {
